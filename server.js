7c9e08e67328535e29ee865f92b477eba4bf85e2
Jonatan Östling <jonatan.ostling@gmail.com>
2016-09-12 15:27:22 +0200

    Angular initialized

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 var express = require('express');
 var app = express();
+
+//server test
+// app.get('/', function(req, res){
+//   res.send("Hello world from server.js")
+// });
+
+//use an html page as the template
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000);
+console.log("Server running on port 3000");
