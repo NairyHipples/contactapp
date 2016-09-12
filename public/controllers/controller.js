77e8bd23ad91fee74cdb7c8a86d7eddb530fc4bc
Jonatan Östling <jonatan.ostling@gmail.com>
2016-09-12 16:33:33 +0200

    data in mongodb, controller receiving data from db

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,26 +1,8 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World");
+    $http.get('/contactlist').success(function (response){
+      console.log("I got the data I requested");
+      $scope.contactlist = response;
+    });
 
-    //dummy data
-    person1 = {
-      name: 'Johan',
-      email: 'johan@email.com',
-      number: '0700-00 00 00'
-    };
-
-    person2 = {
-      name: 'Lisa',
-      email: 'lisa@email.com',
-      number: '0701-00 00 01'
-    };
-
-    person3 = {
-      name: 'Karin',
-      email: 'karin@email.com',
-      number: '0702-00 00 02'
-    };
-
-    var contactlist = [person1, person2, person3];
-    $scope.contactlist = contactlist;
 }]);
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 //server test
 // app.get('/', function(req, res){
@@ -8,6 +10,17 @@ var app = express();
 
 //use an html page as the template
 app.use(express.static(__dirname + "/public"));
+
+//listen for GET request
+app.get('/contactlist', function (req, res){
+  console.log("I received a GET request");
+
+  //get request from mongodb-db
+  db.contactlist.find(function(err, docs){
+    console.log(docs);
+    res.json(docs);
+  });
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
