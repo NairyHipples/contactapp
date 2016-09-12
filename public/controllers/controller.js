189fdaebbcec06537b9c07fa1ddc144f03513bd1
Jonatan Östling <jonatan.ostling@gmail.com>
2016-09-12 17:45:53 +0200

    added update and deselect function

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -28,10 +28,37 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     //remove function
      $scope.remove = function(id){
        console.log(id);
+       //identify chosen contact to remove
        $http.delete('/contactlist/' + id).success(function(response){
          //refreshing the page after clicking remove button
          refresh();
        });
+     };
+
+     //edit function
+     $scope.edit = function(id) {
+       console.log(id);
+       //identify chosen contact to edit
+       $http.get('/contactlist/' + id).success(function(response){
+         //put response in input boxes
+         $scope.contact = response;
+       });
+     };
+
+     //update function
+     $scope.update = function(){
+       console.log($scope.contact._id);
+       //send everything in the input boxes to the server
+       //and refresh the page when update btn is clicked
+       $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+         refresh();
+       });
+     };
+
+     //clear function
+     $scope.deselect = function(){
+       //clear input boxes after calling deselect function
+      $scope.contact = "";
      };
 
 }]);
