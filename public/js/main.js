f1308bd4ec9826978c6233065a80cf94a09db9c5
Jonatan Östling <jonatan.ostling@gmail.com>
2016-09-13 00:48:14 +0200

    layout and styling

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('.edit').click(function(){
+    
+  });
+  // $('.edit').click(function(){
+  //   if($('#add').is(":visible")){
+  //     $('#add').hide();
+  //     $('#update, #clear').show();
+  //
+  //   } else if ($('#update, #clear').is(":visible")){
+  //     $('#add').hide();
+  //     $('#update, #clear').show();
+  //
+  //   }
+  //
+  // });
+  // $('#clear').click(function(){
+  //   $('#update, #clear').hide();
+  //   $('#add').show();
+  //   console.log("clear clicked");
+  //
+  // });
+  // $('#update').click(function(){
+  //   $('#update, #clear').hide();
+  //   $('#add').show();
+  //   console.log("update clicked");
+  //
+  // });
+});
