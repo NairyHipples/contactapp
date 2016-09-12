5fd22f02cd920e30397a31aebc77843500d42f11
Jonatan Östling <jonatan.ostling@gmail.com>
2016-09-12 16:00:32 +0200

    angular controller dummy data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,26 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World");
+
+    //dummy data
+    person1 = {
+      name: 'Johan',
+      email: 'johan@email.com',
+      number: '0700-00 00 00'
+    };
+
+    person2 = {
+      name: 'Lisa',
+      email: 'lisa@email.com',
+      number: '0701-00 00 01'
+    };
+
+    person3 = {
+      name: 'Karin',
+      email: 'karin@email.com',
+      number: '0702-00 00 02'
+    };
+
+    var contactlist = [person1, person2, person3];
+    $scope.contactlist = contactlist;
 }]);
